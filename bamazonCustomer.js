60404f5b5cc5a043db6d9c0532f227d6eaf22038
Louis Kane <louiskane@1234.local>
2016-12-21 22:13:33 -0800

    added command prompts

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -16,13 +16,31 @@ connection.connect(function(err) {
 	console.log("connected as id " + connection.threadId);
 });
 
-var start = function() {
-	inquirer.prompt({
+var startStore = function() {
+	inquirer.prompt([
+	{
 		name:"productID",
 		type:"list",
 		message: "What is the product ID you would like to search?",
 		choices: ["0", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
-	}).then(function(answer){
+	},
+
+	{
+
+		name:"item",
+		type:"input",
+		message:"how many items would you like to purchase?",
+		validade:function(value){
+			if(isNaN(value)===false){
+				return true;
+			}
+				return false;
+		}
+
+	}]).then(function(answer){
+		var userStock = "SELECT stock_quantity, price FROM products WHERE ?"
 		
-	})
-}
+	});
+};
+
+
